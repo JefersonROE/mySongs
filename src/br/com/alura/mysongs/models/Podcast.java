29ac9b03e7d0f2f9e4d91ebda58669563cedc00f
Jeff <jefersonrodriguesoe@gmail.com>
2024-04-25 21:51:58 -0300

    Creating an audio project.

---
+++ src/br/com/alura/mysongs/models/Podcast.java
@@ -0,0 +1,25 @@
+package br.com.alura.mysongs.models;
+
+public class Podcast extends Audio{
+    private String presenter;
+    private String description;
+
+    public String getPresenter() {
+        return presenter;
+    }
+
+    public void setPresenter(String presenter) {
+        this.presenter = presenter;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
